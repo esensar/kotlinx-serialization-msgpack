105ac2e68e614d64933478075e693eef2194f91c
Ensar Sarajčić <es.ensar@gmail.com>
2021-09-09 18:31:16 +0200

    feat: upgrade kotlin version to 1.5.30
    - upgraded kotlin version to 1.5.30
    - upgraded ktlint-gradle version to 10.2.0
    - upgraded dokka-gradle version to 1.5.0
    - upgraded gradle version to 7.2
    
    **NOTES**
    All updates were done to support Gradle 7+

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,8 +1,8 @@
 object Dependencies {
     object Versions {
-        const val kotlin = "1.4.10"
+        const val kotlin = "1.5.30"
         const val serialization = "1.0.1"
-        const val ktlintGradle = "9.4.1"
-        const val dokkaGradle = "1.4.0"
+        const val ktlintGradle = "10.2.0"
+        const val dokkaGradle = "1.5.0"
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,3 @@
 rootProject.name = "kotlinx-serialization-msgpack"
 
 include(":serialization-msgpack")
-
-enableFeaturePreview("GRADLE_METADATA")
