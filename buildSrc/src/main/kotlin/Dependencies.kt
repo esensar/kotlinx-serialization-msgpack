4a56936ebb44693e00f96c118d8186f7b23d7bf5
Ensar Sarajčić <es.ensar@gmail.com>
2021-12-16 10:42:25 +0100

    chore: Upgrade kotlin to 1.6.10 to enable windows cross-compilation

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,6 +1,6 @@
 object Dependencies {
     object Versions {
-        const val kotlin = "1.5.30"
+        const val kotlin = "1.6.10"
         const val serialization = "1.2.2"
         const val datetime = "0.2.1"
         const val ktlintGradle = "10.2.0"
