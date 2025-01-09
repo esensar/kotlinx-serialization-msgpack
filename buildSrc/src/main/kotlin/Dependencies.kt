b4b7c37b41b9dba2d14bdce29950ed99612fb3e6
Ensar Sarajčić <dev@ensarsarajcic.com>
2025-01-09 15:44:15 +0100

    chore: bump kotlin version to 1.9.24

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,6 +1,6 @@
 object Dependencies {
     object Versions {
-        const val kotlin = "1.8.10"
+        const val kotlin = "1.9.24"
         const val serialization = "1.4.0"
         const val datetime = "0.4.0"
         const val ktlintGradle = "10.2.0"
